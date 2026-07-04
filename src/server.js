daaa7c32b4c0b5e2c73ee67848f29e70958a410d
jainish patel <jainishpatel257@gmail.com>
2026-07-04 19:56:50 +0530

    Add backend server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
+
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
